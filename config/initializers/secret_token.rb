344862dc5600bb12664401f5b65073a2b34c727b
Ramesh <ramesheee318@gmail.com>
2012-02-05 22:56:35 +0530

    Created buyall admin project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuyallAdmin::Application.config.secret_token = '174cec64499b0229216df6983e8f757db10228b4a269456139a15781bca857bfd7d87271549b6c3b005d8a731b75e78c8bb3f2c080987cd03fa93aeb0c679710'
